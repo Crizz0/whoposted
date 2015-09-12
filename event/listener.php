fd479e89c99fe03685ea415e28260d7eae894bc5
Crizzo <mail@crizzo.de>
2015-09-12 20:06:13 +0200

    Trying to use get_username_string

--- event/listener.php
+++ event/listener.php
@@ -27,17 +27,20 @@ class listener implements EventSubscriberInterface
 	
 	/** @var \phpbb\db\driver\driver_interface */
 	protected $db;
+	/**  */
+	protected $root_path;
 
 	/**
 	* Constructor
 	*
 	* @param \phpbb\user $user, \phpbb\template\template $template
 	*/
-	public function __construct(\phpbb\user $user, \phpbb\template\template $template, \phpbb\db\driver\driver_interface $db)
+	public function __construct(\phpbb\user $user, \phpbb\template\template $template, \phpbb\db\driver\driver_interface $db, $root_path)
 	{
 		$this->user = $user;
 		$this->template = $template;
 		$this->db = $db;
+		$this->root_path = $root_path;
 
 		$this->user->add_lang_ext('crizzo/whoposted', 'whoposted');
 	}
@@ -55,8 +58,6 @@ class listener implements EventSubscriberInterface
 			'core.viewforum_modify_topicrow'	=> 'modify_replies',
 		);
 	}
-
-
 	/**
 	 * Changes the regex replacement for second pass
 	 *
@@ -66,28 +67,33 @@ class listener implements EventSubscriberInterface
 	 */
 	public function modify_replies($event)
 	{
-		// 1. Themen ID bestimmen
-		// 2. Anzahl der Antworten bestimmen
-		// 3. Autoren des Themas finden
-		// 4. Beiträge je Autor zählen
-		// 5. je Zeile ein Autor + Beitragszahl 
-		// 6. als overlay wie bei Mark Forum Read ausgeben o.ä.
+		if (!function_exists('get_username_string'))
+		{
+			include($this->root_path . 'includes/functions_content.' . $this->php_ext);
+		}
+		// 1. output each line with user + post-count
+		// 2. output in "inline-popup" like in "mark posts read"
 		
 		$topic_row = $event['topic_row'];
 		
 		$topic_id = $topic_row['TOPIC_ID'];
-		
-		
+
 		$sql = 'SELECT COUNT(post_id) AS posts, poster_id
 		FROM phpbb_posts
 		WHERE topic_id = ' . (int) $topic_id . '
 		GROUP BY poster_id
 		ORDER BY posts DESC';
-		
+
 		$result = $this->db->sql_query_limit($sql, 5);
 
 		while($row=$this->db->sql_fetchrow($result)) {
 			var_dump($row);
+			//$post_author_id = $row['poster_id'];
+			$poster_id = $row['poster_id'];
+			$post_count = $row['posts'];
+
+			$display_username = get_username_string('full', $poster_id, $row['username'], $row['user_colour'], $row['post_username']);
+			echo $display_username . 'with ' . $post_count . 'posts<br />';
 		}
 
 		$this->db->sql_freeresult($result);
